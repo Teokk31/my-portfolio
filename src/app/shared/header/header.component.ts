0239b63b84a6b32962ece265bc2bec154f70c0e8
Teo Khai Kiong <khaikiong.teo@digi.com.my>
2020-01-14 23:37:47 +0800

    Content update and bug fixes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isCollapsed: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.isCollapsed = window.innerWidth < 992;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: { target: { innerWidth: number; }; }) {
+    if (event.target.innerWidth > 991) {
+      this.isCollapsed = false;
+    } else {
+      this.isCollapsed = true;
+    }
+  }
+
+  toggleMenu(): void {
+    this.isCollapsed = !this.isCollapsed;
   }
 
 }
